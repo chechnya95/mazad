1868d9f03d6bc65189e05de3e5f3b1b2bbc5c09f
Abdullah Al-Qassabi <squ.sniper@gmail.com>
2021-10-28 14:36:46 +0400

    roles and permissions

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -11,6 +11,16 @@ export class RolesComponent implements OnInit {
 
   token: any;
   roles: any[] = [];
+  permissions: any[] = [];
+
+  permission_name: string = '';
+  role_name: string = '';
+
+  edit_role_id: number = 0;
+  edit_role_name: string = '';
+
+  permissions_checked: any[] = [];
+  role_permissons: any[] = [];
 
   constructor(public utility: UtilitiesService, private api: ApiService) {
     this.utility.show = true;
@@ -30,11 +40,163 @@ export class RolesComponent implements OnInit {
         objects = data;
         this.roles = objects.roles;
 
-        console.log(this.roles)
+        this.getPermissions();
       },
       async error => {
         alert(error);
       }
     );
+  }
+
+  async getPermissions() {
+    this.api.get('users/permissions', this.token).subscribe(
+      async data => {
+        let objects: any = {
+          permissions: []
+        }
+        objects = data;
+        this.permissions = objects.permissions;
+
+        this.permissions.forEach(function (permission) {
+          let split: string[] = permission.name.split(".");
+          permission.obj = split[0];
+          permission.action = split[1];
+        });
+
+        this.getRolePermissions();
+      },
+      async error => {
+        alert(error);
+      }
+    );
+  }
+
+  async getRolePermissions() {
+    this.api.get('users/roles-permissions', this.token).subscribe(
+      async data => {
+        let objects: any = {
+          roles_permissions: []
+        }
+        objects = data;
+        this.role_permissons = objects.roles_permissions;
+      },
+      async error => {
+        alert(error);
+      }
+    );
+  }
+
+  addRole() {
+    let body = {
+      name: this.role_name,
+    }
+
+    if (this.role_name != '') {
+      this.api.post("users/roles", body, this.token).subscribe(
+        async data => {
+          this.getRoles();
+        },
+        async error => {
+          alert("ERROR: cannot connect!");
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  addPermission() {
+    let body = {
+      name: this.permission_name,
+    }
+
+    if (this.permission_name != '') {
+      this.api.post("users/permissions", body, this.token).subscribe(
+        async data => {
+          this.getPermissions();
+        },
+        async error => {
+          alert("ERROR: cannot connect!");
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  editRoleClicked(id: number, name: string) {
+    this.edit_role_name = name;
+    this.edit_role_id = id;
+
+    for (let x = 0; x < this.permissions.length; x++) {
+      for (let y = 0; y < this.role_permissons.length; y++) {
+        if (this.role_permissons[y].role_id == this.edit_role_id) {
+          if (this.permissions[x].id == this.role_permissons[y].permission_id) {
+            this.permissions[x].checked = true;
+          }
+        }
+      }
+    }
+  }
+
+  checkBoxChecked(permision: any) {
+    if (permision.checked) {
+      let body = { role_id: this.edit_role_id, permission_id: permision.id }
+      this.api.post("users/add-permission", body, this.token).subscribe(
+        async data => {
+          //console.log('ok');
+        },
+        async error => {
+          alert("ERROR: cannot connect!");
+          console.log(error);
+        }
+      );
+    }
+    else {
+      this.api.delete("users/delete-role-permission/" + this.edit_role_id + "/" + permision.id, this.token).subscribe(
+        async data => {
+          //console.log('deleted');
+        },
+        async error => {
+          alert("ERROR: cannot connect!");
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  updateRole(id: number) {
+    let role_name = this.roles.find(i => i.id == id).name;
+
+    if (role_name != this.edit_role_name) {
+      alert('hi')
+      let body = { name: this.edit_role_name }
+
+      this.api.update("users/roles/" + this.edit_role_id, body, this.token).subscribe(
+        async data => {
+          //this.updateRolesPermissions(this.edit_role_id);
+          this.getRoles();
+        },
+        async error => {
+          alert("ERROR: cannot connect!");
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  updateRolesPermissions(this_role_id: number) {
+    let permissions = this.permissions.filter(i => i.checked == true);
+
+    for (let i = 0; i < permissions.length; i++) {
+      let body = { role_id: this_role_id, permission_id: permissions[i].id }
+      this.api.post("users/add-permission", body, this.token).subscribe(
+        async data => {
+          console.log('ok');
+        },
+        async error => {
+          alert("ERROR: cannot connect!");
+          console.log(error);
+        }
+      );
+    }
   }
 }
