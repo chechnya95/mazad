3adc5e9c4bb08f9772d3403c3d81571692bbf0d7
Abdullah Al-Qassabi <squ.sniper@gmail.com>
2021-11-04 11:26:58 +0400

    items page finished

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -16,6 +16,10 @@ export class ItemsComponent implements OnInit {
   owners: any[] = [];
   item_status: any[] = [];
 
+  templates: any[] = [];
+
+  type: string = 'empty';
+
   item = {
     code: null,
     details: null,
@@ -121,6 +125,20 @@ export class ItemsComponent implements OnInit {
       async data => {
         let objects = JSON.parse(JSON.stringify(data))
         this.categories = objects['categories'];
+
+        this.getTemplates();
+      },
+      async error => {
+        alert(error);
+      }
+    );
+  }
+
+  async getTemplates() {
+    this.api.get('auction_templates/', this.token).subscribe(
+      async data => {
+        let objects = JSON.parse(JSON.stringify(data));
+        this.templates = objects['auction_templates'];
       },
       async error => {
         alert(error);
@@ -158,15 +176,20 @@ export class ItemsComponent implements OnInit {
       terms: { 'en': this.item.terms_en, 'ar': this.item.terms_ar }
     }
 
-    this.api.post("items/", body, this.token).subscribe(
-      async data => {
-        this.getItems();
-      },
-      async error => {
-        alert("ERROR: cannot connect!");
-        console.log(error);
-      }
-    );
+    if (this.item.current_price && this.item.item_status) {
+      this.api.post("items/", body, this.token).subscribe(
+        async data => {
+          this.getItems();
+        },
+        async error => {
+          alert("ERROR: cannot connect!");
+          console.log(error);
+        }
+      );
+    }
+    else {
+      alert('Please check all fields!')
+    }
   }
 
   deleteItem(id: number) {
@@ -212,4 +235,60 @@ export class ItemsComponent implements OnInit {
     );
   }
 
+  continue(type: string) {
+    this.type = type;
+  }
+
+  teplateChecked(template: any) {
+    this.item = template;
+
+    this.item.title_ar = template.title['ar'];
+    this.item.title_en = template.title['en'];
+    this.item.description_ar = template.description['ar'];
+    this.item.description_en = template.description['en'];
+    this.item.terms_ar = template.terms['ar'];
+    this.item.terms_en = template.terms['en'];
+
+    // get start date
+    var start_date = new Date(template.start_date);
+    var month_1 = (start_date.getMonth() + 1).toString();
+    var day_1 = (start_date.getDate()).toString();
+    var hour_1 = (start_date.getHours()).toString();
+    var mins_1 = (start_date.getMinutes()).toString();
+
+    if (+month_1 < 10)
+      month_1 = '0' + month_1;
+
+    if (+day_1 < 10)
+      day_1 = '0' + day_1;
+
+    if (+hour_1 < 10)
+      hour_1 = '0' + hour_1;
+
+    if (+mins_1 < 10)
+      mins_1 = '0' + mins_1;
+
+    this.item.start_date = start_date.getFullYear() + '-' + month_1 + '-' + day_1 + 'T' + hour_1 + ':' + mins_1;
+
+    // get end date
+    var end_date = new Date(template.end_date);
+    var month_2 = (end_date.getMonth() + 1).toString();
+    var day_2 = (end_date.getDate()).toString();
+    var hour_2 = (end_date.getHours()).toString();
+    var mins_2 = (end_date.getMinutes()).toString();
+
+    if (+month_2 < 10)
+      month_2 = '0' + month_2;
+
+    if (+day_2 < 10)
+      day_2 = '0' + day_2;
+
+    if (+hour_2 < 10)
+      hour_2 = '0' + hour_2;
+
+    if (+mins_2 < 10)
+      mins_2 = '0' + mins_2;
+
+    this.item.end_date = end_date.getFullYear() + '-' + month_2 + '-' + day_2 + 'T' + hour_2 + ':' + mins_2;
+  }
 }
