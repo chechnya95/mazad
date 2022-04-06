4a7485fbad5a5d315932400084f40b4dce9f4064
Abdullah Al-Qassabi <squ.sniper@gmail.com>
2022-04-06 11:28:40 +0400

    Update item-pipe.pipe.ts

--- src/app/pipes/item-pipe.pipe.ts
+++ src/app/pipes/item-pipe.pipe.ts
@@ -7,7 +7,7 @@ export class ItemPipePipe implements PipeTransform {
 
   transform(value: any, input: any): any {
     if (input) {
-      return value.filter(val => val.code.toLowerCase().indexOf(input.toLowerCase()) >= 0 || val.item_status.toLowerCase().indexOf(input.toLowerCase()) >= 0);
+      return value.filter(val => val.id.toLowerCase().indexOf(input.toLowerCase()) >= 0 || val.code.toLowerCase().indexOf(input.toLowerCase()) >= 0 || val.item_status.toLowerCase().indexOf(input.toLowerCase()) >= 0 || val.title.en.toString().indexOf(input.toString()) >= 0 || val.title.ar.toString().indexOf(input) >= 0);
     } else {
       return value;
     }
