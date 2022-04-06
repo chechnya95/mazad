a032160f1e2a0034be666ec1f37b1b320f1894c2
Abdullah Al-Qassabi <squ.sniper@gmail.com>
2022-04-06 11:48:57 +0400

    Update user-pipe.pipe.ts

--- src/app/pipes/user-pipe.pipe.ts
+++ src/app/pipes/user-pipe.pipe.ts
@@ -7,7 +7,7 @@ export class UserPipePipe implements PipeTransform {
 
   transform(value: any, input: any): any {
     if (input) {
-      return value.filter(val => val.phone != null && val.phone.toString().indexOf(input) >= 0 || val.email.toLowerCase().indexOf(input.toLowerCase()) >= 0 || val.name.toLowerCase().indexOf(input.toLowerCase()) >= 0);
+      return value.filter(val => val.phone != null && val.phone.toString().indexOf(input) >= 0 || val.email != null && val.email.toLowerCase().indexOf(input.toLowerCase()) >= 0 || val.name != null && val.name.toLowerCase().indexOf(input.toLowerCase()) >= 0);
     } else {
       return value;
     }
