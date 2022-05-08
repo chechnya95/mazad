480d95cfa51f16f36dcab034c13dac2ebf3a3993
Abdullah Al.Qassabi <squ.sniper@gmail.com>
2022-05-08 19:54:20 +0400

    auction details items and templates lists

--- src/app/auctions/auction-details/auction-details.component.ts
+++ src/app/auctions/auction-details/auction-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-auction-details',
@@ -9,14 +10,44 @@ import { Router } from '@angular/router';
 export class AuctionDetailsComponent implements OnInit {
 
   auction: any;
+  token: any;
+  items: any[] = [];
+  templates: any[] = [];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private api: ApiService) {
+    this.token = localStorage.getItem('access_token');
+  }
 
   ngOnInit(): void {
-    let object = localStorage.getItem('auction') ? JSON.parse(localStorage.getItem('auction')) : null;
-    
-    if (object) { this.auction = object; }
+    this.auction = localStorage.getItem('auction') ? JSON.parse(localStorage.getItem('auction')) : null;
+
+    if (this.auction) { this.getItems(); }
     else { this.router.navigate(['auctions']); }
   }
 
+  async getItems() {
+    const sub = this.api.get('items/auction/' + this.auction.id, this.token).subscribe(
+      async data => {
+        let objects = JSON.parse(JSON.stringify(data));
+        this.items = objects['items']['items'];
+      },
+      async error => {
+        alert(error);
+      }
+    );
+
+    sub.add(() => { this.getTemplates(); });
+  }
+
+  async getTemplates() {
+    this.api.get('auction_templates/auction/' + this.auction.id, this.token).subscribe(
+      async data => {
+        let objects = JSON.parse(JSON.stringify(data));
+        this.templates = objects['auction_templates']['auction_templates'];
+      },
+      async error => {
+        alert(error);
+      }
+    );
+  }
 }
