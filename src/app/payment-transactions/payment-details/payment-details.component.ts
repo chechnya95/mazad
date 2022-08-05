448afbc019ef00d857158b0892dfd780e4b240b7
Abdullah Al.Qassabi <squ.sniper@gmail.com>
2022-08-05 10:51:58 +0400

    fixes

--- src/app/payment-transactions/payment-details/payment-details.component.ts
+++ src/app/payment-transactions/payment-details/payment-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
 
@@ -13,15 +13,35 @@ export class PaymentDetailsComponent implements OnInit {
   payment: any;
   token: any;
 
-  constructor(private router: Router, private api: ApiService, public utility: UtilitiesService) {
+  constructor(private router: Router, private api: ApiService, public utility: UtilitiesService, private route: ActivatedRoute) {
     this.token = localStorage.getItem('access_token');
     this.utility.title = 'Payment Details';
   }
 
   ngOnInit(): void {
-    this.payment = localStorage.getItem('payment') ? JSON.parse(localStorage.getItem('payment')) : null;
+    this.route.queryParams.subscribe(params => {
+      let id = params['id'] != null ? params['id'] : null;
 
-    if (this.payment) {  }
-    else { this.router.navigate(['auctions']); }
+      if (id) this.getPayment(id);
+      else {
+        this.payment = localStorage.getItem('payment') ? JSON.parse(localStorage.getItem('payment')) : null;
+        if (this.payment) { }
+        else { this.router.navigate(['payment-transactions']); }
+      }
+    });
+  }
+
+  getPayment(id: any) {
+    const sub = this.api.get('payments/' + id, this.token).subscribe(
+      async data => {
+        let objects = JSON.parse(JSON.stringify(data));
+        this.payment = objects['payments'][0];
+      },
+      async error => { }
+    );
+
+    sub.add(() => {
+      if (!this.payment) { this.router.navigate(['payment-transactions']); }
+    });
   }
 }
