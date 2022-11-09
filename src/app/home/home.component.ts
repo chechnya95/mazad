324e6c4008144fdef2b9fefd54033901e0cd33fd
Abdullah Al.Qassabi <squ.sniper@gmail.com>
2022-11-09 17:39:23 +0400

    updates statistics home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { UtilitiesService } from '../services/utilities.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,11 @@ import { UtilitiesService } from '../services/utilities.service';
 export class HomeComponent implements OnInit {
 
   token: any;
-  roles: any[] = [];
+  categories: any[] = [];
+  statuses: any[] = [];
+  categories_total_items = 0;
+
+  Swal = require('sweetalert2')
 
   constructor(public utility: UtilitiesService, private api: ApiService) {
     this.utility.show = true;
@@ -23,5 +28,91 @@ export class HomeComponent implements OnInit {
       localStorage.setItem('foo', 'no reload');
       window.location.reload();
     }
+
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.utility.loader = true;
+
+    const sub = this.api.get('dashboards/items/category', this.token).subscribe(
+      async data => {
+        let objects = JSON.parse(JSON.stringify(data));
+        this.categories = objects['items_statistics'];
+
+        this.categories.forEach((item) => {
+          this.categories_total_items += +item.count;
+        })
+      },
+      async error => {
+        Swal.fire({
+          title: 'Oops...',
+          text: 'Something went wrong!'
+        })
+        console.log(error);
+      }
+    );
+
+    sub.add(() => { this.getStatus(); });
+  }
+
+  getStatus() {
+    const sub = this.api.get('dashboards/items/item_status', this.token).subscribe(
+      async data => {
+        let objects = JSON.parse(JSON.stringify(data));
+        this.statuses = objects['items_statistics'];
+      },
+      async error => {
+        Swal.fire({
+          title: 'Oops...',
+          text: 'Something went wrong!'
+        })
+        console.log(error);
+      }
+    );
+
+    sub.add(() => { this.utility.loader = false; });
+  }
+
+  getClassColor(i: number) {
+    if (i % 4 == 0)
+      return 'text-warning';
+
+    if (i % 4 == 1)
+      return 'text-success'
+
+    if (i % 4 == 2)
+      return 'text-danger'
+
+    if (i % 4 == 3)
+      return 'text-primary'
+  }
+
+  getStatusClassColor(i: number) {
+    if (i % 4 == 0)
+      return 'bg-light-warning';
+
+    if (i % 4 == 1)
+      return 'bg-light-success'
+
+    if (i % 4 == 2)
+      return 'bg-light-danger'
+
+    if (i % 4 == 3)
+      return 'bg-light-primary'
+  }
+
+  getIconClassColor(i: number) {
+    if (i % 4 == 0)
+      return 'svg-icon-warning';
+
+    if (i % 4 == 1)
+      return 'svg-icon-success'
+
+    if (i % 4 == 2)
+      return 'svg-icon-danger'
+
+    if (i % 4 == 3)
+      return 'svg-icon-primary'
   }
 }
