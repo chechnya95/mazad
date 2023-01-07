e638fec6b05bd66fed4e1a28bbfad80c39b5c9e6
Mazin Al Saadi <maz1987in@gmail.com>
2023-01-07 12:19:51 +0400

    add new url

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://beta-api.mazad.om/mazad/api/v1/',
-  server: 'https://beta-api.mazad.om/'
+  API_URL: 'http://api.mazad-beta.decoil.co/mazad/api/v1/',
+  server: 'http://api.mazad-beta.decoil.co/'
 };
