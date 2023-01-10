9ca605438771744e073771f338891c314c92616f
Abdullah Al.Qassabi <squ.sniper@gmail.com>
2023-01-10 10:26:51 +0400

    upload id image on user profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { UtilitiesService } from '../services/utilities.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-profile',
@@ -12,6 +13,9 @@ export class ProfileComponent implements OnInit {
   token: any;
   id: any;
   user: any;
+  image: any
+
+  Swal = require('sweetalert2')
 
   constructor(public utility: UtilitiesService, private api: ApiService) {
     this.utility.show = true;
@@ -38,9 +42,29 @@ export class ProfileComponent implements OnInit {
         this.user.avatar = this.user.user_details ? this.user.user_details['name_en'].charAt(0) : this.user['username'].charAt(0);
         this.user.name = this.user.user_details ? this.user.user_details['name_en'] : '';
       },
-      async error => {
-        alert(error);
-      }
+      async error => { }
     );
+  }
+
+  imageChange(event: any) {
+    let fileList: FileList = event.target.files;
+    this.image = fileList[0];
+  }
+
+  addIdImage() {
+    let formData: FormData = new FormData();
+
+    if (this.image) {
+      formData.append('id_card_image', this.image, this.image.name);
+      this.api.post_form("users/image", formData, this.token).subscribe(
+        async data => { },
+        async error => {
+          Swal.fire({
+            title: 'Error...',
+            text: 'ERROR: cannot connect!\nPlease try again later.'
+          });
+        }
+      );
+    }
   }
 }
