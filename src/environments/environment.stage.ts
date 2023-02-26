24a68e8522069f0bf6f36d45066448b748734cd9
Mazin Al Saadi <maz1987in@gmail.com>
2023-02-26 18:38:21 +0400

    Update src/environments/environment.stage.ts

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://api.mazad-beta.decoil.co/mazad/api/v1/',
-  server: 'http://api.mazad-beta.decoil.co/'
+  API_URL: 'http://api.mazad-beta.mazad.om/mazad/api/v1/',
+  server: 'http://api.mazad-beta.mazad.om/'
 };
