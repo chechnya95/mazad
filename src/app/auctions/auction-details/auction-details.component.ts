efe33294aeeac5521c77c1686d45a11532080565
Abdullah Al.Qassabi <squ.sniper@gmail.com>
2023-03-02 08:40:11 +0400

    export import - not completed

--- src/app/auctions/auction-details/auction-details.component.ts
+++ src/app/auctions/auction-details/auction-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { ApiService } from 'src/app/services/api.service';
 import Swal from 'sweetalert2'
 
@@ -24,7 +25,7 @@ export class AuctionDetailsComponent implements OnInit {
 
   Swal = require('sweetalert2')
 
-  constructor(private router: Router, private api: ApiService) {
+  constructor(private router: Router, private api: ApiService, public translate: TranslateService) {
     this.token = localStorage.getItem('access_token');
   }
 
@@ -110,5 +111,71 @@ export class AuctionDetailsComponent implements OnInit {
       else
         this.errorMessage = true;
     }
+  }
+
+  excel: any;
+  imageChange(event: any) {
+    let fileList: FileList = event.target.files;
+    this.excel = fileList[0];
+  }
+
+  export(id: any) {
+    let lang = this.translate.currentLang == 'en' || this.translate.currentLang == null ? 'en' : 'ar';
+
+    Swal.fire({
+      title: lang == 'en' ? 'Are you sure to export Auction?' : 'هل تريد المواصلة لتحميل مزاد؟',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: lang == 'en' ? 'Yes' : 'نعم',
+      cancelButtonText: lang == 'en' ? 'Cancel' : 'الغاء'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.api.get('auctions/export/' + id, this.token).subscribe(
+          res => { },
+          err => {
+            Swal.fire(
+              'Error!',
+              'Could not send your request!'
+            );
+          });
+      }
+    });
+  }
+
+  import(id: any) {
+    let lang = this.translate.currentLang == 'en' || this.translate.currentLang == null ? 'en' : 'ar';
+
+    Swal.fire({
+      title: lang == 'en' ? 'Select file' : 'اختر ملف',
+      html:
+        '<input type="file" class="form-control form-control-solid mb-3 mb-lg-0" id="image" accept="xlsx,xls" (change)="imageChange($event)"  />',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: lang == 'en' ? 'Yes' : 'نعم',
+      cancelButtonText: lang == 'en' ? 'Cancel' : 'الغاء'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let formData: FormData = new FormData();
+
+        if (this.excel) {
+          formData.append('file', this.excel, this.excel.name);
+          this.api.post('auctions/import/' + id, null, this.token).subscribe(
+            res => {
+              Swal.fire({
+                title: 'Success',
+                text: 'Done!'
+              })
+            },
+            err => {
+              Swal.fire(
+                'Error!',
+                'Could not send your request!'
+              );
+            });
+        }
+      }
+    });
   }
 }
