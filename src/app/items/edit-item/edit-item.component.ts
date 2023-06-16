abd90581bfb9158044589057996d1ba1a3e29bc5
Mazin Al Saadi <maz1987in@gmail.com>
2023-06-16 18:53:22 +0400

    fix dates

--- src/app/items/edit-item/edit-item.component.ts
+++ src/app/items/edit-item/edit-item.component.ts
@@ -333,12 +333,11 @@ export class EditItemComponent implements OnInit {
 
   OnSubmit() {
 
-    let date = new Date(this.item.end_date)
-    let end_date = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}+0400`;
-    console.log(`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}+0400`);
-
-    console.log(end_date);
-    console.log(date.toLocaleString('en-US', { timeZone: 'Asia/Muscat' }));
+    let date_s = new Date(this.item.start_date)
+    let start_date = `${date_s.getDate()}-${date_s.getMonth() + 1}-${date_s.getFullYear()} ${date_s.getHours()}:${date_s.getMinutes()}+0400`;
+    
+    let date_e = new Date(this.item.end_date)
+    let end_date = `${date_e.getDate()}-${date_e.getMonth() + 1}-${date_e.getFullYear()} ${date_e.getHours()}:${date_e.getMinutes()}+0400`;
 
     const body = JSON.stringify({
       code: this.item.code,
@@ -347,7 +346,7 @@ export class EditItemComponent implements OnInit {
       owner_code: this.item.owner_code,
       attachments: this.item.attachments,
       deposit: this.item.deposit,
-      start_date: this.item.start_date,
+      start_date: start_date,
       end_date: end_date,
       latitude: this.item.latitude,
       longtitude: this.item.longtitude,
