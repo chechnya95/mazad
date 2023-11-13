73fbfcf037fbc84621cc5655b0f4143d1d0ca6a6
Mazin Al Saadi <maz1987in@gmail.com>
2023-11-13 11:20:27 +0400

    Update file auctions.component.ts

--- src/app/auctions/auctions.component.ts
+++ src/app/auctions/auctions.component.ts
@@ -14,6 +14,7 @@ import { MdEditorOption } from 'ngx-markdown-editor';
   templateUrl: './auctions.component.html',
   styleUrls: ['./auctions.component.css']
 })
+
 export class AuctionsComponent implements OnInit {
 
   token: any;
