8062cea6e552d5166b000bf537a185867d52ba0a
maz1987in <maz1987in@gmail.com>
2024-01-18 12:28:24 +0400

    add pagination to template-details

--- src/app/auction-templates/template-details/template-details.component.ts
+++ src/app/auction-templates/template-details/template-details.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
 import { TranslateService } from '@ngx-translate/core';
+import {Sort} from '@angular/material/sort';
+import { HttpParams } from '@angular/common/http';
+import Swal from 'sweetalert2'
+import { LegacyPageEvent as PageEvent } from '@angular/material/legacy-paginator';
 
 @Component({
   selector: 'app-template-details',
@@ -14,6 +18,7 @@ export class TemplateDetailsComponent implements OnInit {
   token: any;
   item: any;
   keys: any;
+  template_id: any;
 
   items: any[] = [];
 
@@ -24,18 +29,28 @@ export class TemplateDetailsComponent implements OnInit {
   end_date: any;
   errorMessage: boolean = false;
   successMessage: boolean = false;
+  filter_config: any;
 
   constructor(public utility: UtilitiesService, private api: ApiService, private route: ActivatedRoute, private router: Router, public translate: TranslateService) {
     this.utility.show = true;
     this.utility.loader = false;
     this.utility.title = 'Template Details';
     this.token = localStorage.getItem('access_token');
+    this.filter_config = {
+      itemsPerPage: 10,
+      currentPage: 1,
+      totalItems: 0,
+      sort: null,
+      sort_order: 'asc',
+      pageSizeOptions: [5, 10, 25, 100]
+    };
   }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       let id = params['id'] != null ? params['id'] : null;
       if (id) {
+        this.template_id = id;
         let objects = localStorage.getItem('templates') ? JSON.parse(localStorage.getItem('templates')) : null;
         if (objects) {
           this.item = objects.find(i => i.id === id);
@@ -49,10 +64,11 @@ export class TemplateDetailsComponent implements OnInit {
   }
 
   getTemplateItems(id: any) {
-    const sub = this.api.get('items/template/' + id, this.token).subscribe(
+    const sub = this.api.get('items/template/' + id, this.token, { params: this.getHttpParams()}).subscribe(
       async data => {
         let objects = JSON.parse(JSON.stringify(data));
         this.items = objects['items']['items'];
+        this.filter_config.totalItems = objects['items']['filters']['total_results'];
       },
       async error => { }
     );
@@ -103,4 +119,26 @@ export class TemplateDetailsComponent implements OnInit {
       async error => {  this.errorMessage = true; }
     );
   }
+  getHttpParams() {
+    let params = new HttpParams();
+    params = params.append('page', this.filter_config.currentPage.toString());
+    params = params.append('per_page', this.filter_config.itemsPerPage.toString());
+    if (this.filter_config.sort) {
+      params = params.append('sort', this.filter_config.sort);
+      params = params.append('sort_order', this.filter_config.sort_order);
+    }
+    return params;
+  }
+  pageChangeEvent(event: PageEvent) {
+    this.filter_config.currentPage = event.pageIndex + 1;
+    this.filter_config.itemsPerPage = event.pageSize;
+    this.getTemplateItems(this.template_id);
+  }
+  
+  sortData(sort: Sort) {
+    this.filter_config.sort = sort.active;
+    this.filter_config.sort_order = sort.direction;
+    this.getTemplateItems(this.template_id);
+  }
+
 }
